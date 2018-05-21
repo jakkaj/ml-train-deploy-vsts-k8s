afe26b8e17b233a61c59194b4dcf8184b50b2bd5
Jordan Knight <jakkaj@gmail.com>
2018-05-21 17:15:51 +1000

    WIP values

--- Source/scoring/init/waiter.sh
+++ Source/scoring/init/waiter.sh
@@ -4,5 +4,5 @@ while [ ! -f $MODELFOLDER/complete.txt ]; do
     sleep 2
 done
 value=$(<$MODELFOLDER/complete.txt)
-#echo "model_score $value" | curl --data-binary @- http://prometheus-pushgateway.prometheus.svc.cluster.local:9091/metrics/job/some_job
+# echo "model_score $value" | curl --data-binary @- http://prometheus-pushgateway.prometheus.svc.cluster.local:9091/metrics/job/some_job
 echo "File found"
