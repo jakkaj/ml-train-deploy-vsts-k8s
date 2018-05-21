d247f62a1bf063c621c192417899b2a20e61c3ad
Jordan Knight <jakkaj@gmail.com>
2018-05-21 19:10:35 +1000

    output build number from scoring site

--- Source/scoring/site/index.js
+++ Source/scoring/site/index.js
@@ -5,11 +5,12 @@ const path = require('path');
 
 app.get('/', (req, res) => {
     var modelFolder = process.env.MODELFOLDER;
+    var buildNumber = process.env.BUILDNUMBER;
     //this is the complete file - we'd read the model here perhaps... it's just a demo!
     var file = path.join(modelFolder, "complete.txt");
     if(fs.existsSync(file)){
         var contents = fs.readFileSync(file, 'utf-8');
-        res.send(`Score: ${contents}`);
+        res.send(`Score: ${contents}\r\nBuild Number:${buildNumber}`);
     }else{
         res.send("Error, could not find the complete.txt file. ")
     }
